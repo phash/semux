b3c5baece8184cef3de22bb5cf113919da1a9fdf
Phash <mroedig@gmail.com>
2017-12-17 13:47:25 +0100

    refactor ActionListener
    pulling out the actions from UI
    extracting the validation of tx behaviour
    
    remove unnecessary constant
    
    Changed the format of the testable address
    
    extracted another method
    
    got rid of magic number
    
    expanded it more
    
    HexDecode 0x
    
    Net: close peer client
    
    transient action listener

--- src/main/java/org/semux/config/Config.java
+++ src/main/java/org/semux/config/Config.java
@@ -44,7 +44,7 @@ public interface Config {
     short networkVersion();
 
     /**
-     * Returns the max block size in bytes.
+     * Returns the max block size in transactions.
      * 
      * @return
      */
@@ -56,7 +56,7 @@ public interface Config {
      * @return
      */
     int maxTransferDataSize();
-
+    
     /**
      * Returns the min transaction fee.
      * 
